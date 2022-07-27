bd722bdf5b78c0938b0193b840b5bf084175eae6
Luana <luanaff-2008@hotmail.com>
2022-07-27 20:38:27 -0300

    feat: change name seedrs user

--- app/backend/src/database/models/User.ts
+++ app/backend/src/database/models/User.ts
@@ -2,40 +2,40 @@ import { Model, STRING, INTEGER } from 'sequelize';
 import db from '.';
 
 class Users extends Model {
-    id: number;
-    username: string;
-    role: string;
-    email: string;
-    password: string; 
+  id: number;
+  username: string;
+  role: string;
+  email: string;
+  password: string;
 }
-    Users.init({
-        id: {
-            allowNull: false,
-            autoIncrement: true,
-            primaryKey: true,
-            type: INTEGER
-          },
-        username: {
-            allowNull: false,
-            type: STRING(255),
-          },
-          role: {
-            allowNull: false,
-            type: STRING(255),
-          },
-          email: {
-            allowNull: false,
-            type: STRING(255)
-          },
-          password: {
-            allowNull: false,
-            type: STRING(255)
-          },
+Users.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  username: {
+    allowNull: false,
+    type: STRING(255),
+  },
+  role: {
+    allowNull: false,
+    type: STRING(255),
+  },
+  email: {
+    allowNull: false,
+    type: STRING(255),
+  },
+  password: {
+    allowNull: false,
+    type: STRING(255),
+  },
 }, {
-    underscored: true,
-    sequelize: db,
-    modelName: 'Users',
-    timestamps: false,
-})
+  underscored: true,
+  sequelize: db,
+  modelName: 'Users',
+  timestamps: false,
+});
 
 export default Users;
