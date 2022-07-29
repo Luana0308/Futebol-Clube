91b43f9205011701266275bf03b9587f368d840a
Luana <luanaff-2008@hotmail.com>
2022-07-29 20:33:58 -0300

    filter routes matches

--- app/backend/src/controllers/matches.controllers.ts
+++ app/backend/src/controllers/matches.controllers.ts
@@ -1,11 +1,17 @@
 import { Request, Response } from 'express';
 import service from '../services/matches.service';
 
-const getAllMatches = async (_req: Request, res: Response): Promise<Response> => {
-  const matches = await service.getAllMatches();
+const getAllMatches = async (req: Request, res: Response): Promise<Response> => {
+  const matches = await service.getAllMatches(req.query);
   return res.status(200).json(matches);
 };
 
+// const getMatchesByQuery = async (req: Request, res: Response): Promise<Response | void> => {
+//   const matchesFilter = await service.getMatchesByQuery(req.query);
+//   return res.status(200).json(matchesFilter);
+// };
+
 export default {
   getAllMatches,
+  // getMatchesByQuery,
 };
--- app/backend/src/services/matches.service.ts
+++ app/backend/src/services/matches.service.ts
@@ -1,12 +1,24 @@
 import Teams from '../database/models/Teams';
 import Matches from '../database/models/Matches';
+import { IMacthesFilter } from '../interfaces/Matches';
 
-const getAllMatches = async () => {
+const getAllMatches = async (query: IMacthesFilter) => {
+  const include = [
+    { model: Teams, as: 'teamHome', attributes: ['teamName'] },
+    { model: Teams, as: 'teamAway', attributes: ['teamName'] },
+  ];
+
+  if (query.inProgress !== undefined) {
+    const value = (query.inProgress === 'true');
+
+    const filterMatches = await Matches.findAll({
+      where: { inProgress: +value },
+      include,
+    });
+    return filterMatches;
+  }
   const matches = await Matches.findAll({
-    include: [
-      { model: Teams, as: 'teamHome', attributes: ['teamName'] },
-      { model: Teams, as: 'teamAway', attributes: ['teamName'] },
-    ],
+    include,
   });
   return matches;
 };
